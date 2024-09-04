c64f01a0df62d570983b36dbf8993b34f6fbbef7
flavius <flaviuscojocaru19@gmail.com>
2024-09-04 12:01:53 +0300

    phase 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date());
+
+  const decreaseStep = () => {
+    setStep(prev => prev - 1);
+  }
+
+  const increaseStep = () => {
+    setStep(prev => prev + 1);
+  }
+
+  const decreaseCount = () => {
+    setCount(prev => prev - step)
+    setDate(prev => (new Date(prev)).getTime() - step * 86400000)
+  }
+
+  const increaseCount = () => {
+    setCount(prev => prev + step)
+    setDate(prev => (new Date(prev)).getTime() + step * 86400000)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: "flex",
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }} >
+      <div>
+        <button onClick={decreaseStep}>-</button>
+        Step: {step}
+        <button onClick={increaseStep}>+</button>
+      </div>
+      <div>
+      <button onClick={decreaseCount}>-</button>
+        Count: {count}
+        <button onClick={increaseCount}>+</button>
+      </div>
+      {(new Date(date)).getDay() === new Date().getDay() && <p>Today is {(new Date(date)).toDateString()}</p>}
+      {(new Date(date)).getDay() < new Date().getDay() && <p>{count} days ago was {(new Date(date)).toDateString()}</p>}
+      {(new Date(date)).getDay() > new Date().getDay() && <p>{count} days from today is {(new Date(date)).toDateString()}</p>}
     </div>
   );
 }
